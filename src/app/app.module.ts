9f78810dd7c58af04b07e18b2f8ec3c41c8892d0
Indira Rojas <inroes.soluciones@gmail.com>
2021-01-31 20:19:54 -0500

    Lo que realicé en la clase 03 - 27/01/2021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { BookExampleComponent } from './components/book-example/book-example.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { BookListExampleComponent } from './components/book-list-example/book-list-example.component';
+import { BookSelectCountComponent } from './components/book-select-count/book-select-count.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent
+    HelloComponent,
+    BookExampleComponent,
+    HomePageComponent,
+    BookListExampleComponent,
+    BookSelectCountComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/book.ts
@@ -0,0 +1,10 @@
+export interface IBook{
+    id: number;
+    name: string;
+    isbn: string;
+    image: string;
+    language: 'es' | 'en';
+    publishedDate: string;
+    publisher: string;
+}
+
