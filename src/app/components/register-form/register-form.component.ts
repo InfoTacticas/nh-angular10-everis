116ee0988c19622bc1d47a6b903a036ef0ae7a8d
Indira Rojas <inroes.soluciones@gmail.com>
2021-02-07 20:23:26 -0500

    Mi Clase 05- Formularios Reactivos con FormBuilder y control de errror, siempre se debe usar FormBuilder

---
+++ src/app/components/register-form/register-form.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators  } from '@angular/forms'
+
+@Component({
+  selector: 'app-register-form',
+  templateUrl: './register-form.component.html',
+  styleUrls: ['./register-form.component.scss']
+})
+export class RegisterFormComponent implements OnInit {
+  registerForm: FormGroup;
+ 
+  /** FormBuilder es un servicio --> inyeccion de dependencias en el constructor : PRIVATE */
+  constructor(
+    private fb: FormBuilder
+  ) { 
+
+  }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(){
+    const formData = this.buildForm();
+    this.registerForm = this.fb.group(formData);
+  }
+
+  buildForm(){
+    const formData = {
+      firstName: [null, Validators.required],
+      lastName: [null, Validators.required],
+      username: [null, Validators.required, Validators.maxLength(12)],
+      password: [null, Validators.required],
+      confirmPassword: [null, Validators.required]
+    }
+    return formData;
+  }
+
+  get f(){
+    return this.registerForm.controls;
+  }
+
+}
